f9a90f2e44b170dc44b6adcf65f22722f0094577
Ali-Mehdi <67590740+Ali-Mehdi@users.noreply.github.com>
2026-02-05 04:42:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+
+function heartRain() {
+  const container = document.getElementById('heartRain');
+  container.style.display = 'block';
+
+  const hearts = ['❤️','💖','💕','💘','💞','🌹'];
+
+  setInterval(() => {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.innerText = hearts[Math.floor(Math.random() * hearts.length)];
+
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = (Math.random() * 3 + 2) + 's';
+
+    container.appendChild(heart);
+
+    setTimeout(() => heart.remove(), 6000);
+  }, 200);
+}
